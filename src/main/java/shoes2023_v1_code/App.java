ae717e7d83acebfa11a60e3d6fcea70122003be3
ckala <ckala@gmail.com>
2023-03-30 16:02:44 +0530

    myntra shoes code dev by junior dev

---
+++ src/main/java/shoes2023_v1_code/App.java
@@ -0,0 +1,13 @@
+package shoes2023_v1_code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
